17a4e83b65dd08ccb910dba90fd6fa52baa6b80d
Nidhi Sikarwar <snidhi@gmail.com>
2018-02-10 13:14:47 -0800

    added try except for input file. wrote a function to get the values from input

--- src/donation-analytics.py
+++ src/donation-analytics.py
@@ -9,6 +9,25 @@ def percentile(arr,p):
     else:    
         return arr[int((float(p)/100)*n) ]
 
+def getValues(line):
+    
+    try:
+        all_values = line.split("|")
+                
+        recipient = all_values[0]
+        donor_name = all_values[7]
+        zip_code = int(all_values[10][:5])
+        year = int(all_values[13][-4:])
+        amt = int(all_values[14])
+        other_id = all_values[15]    
+        
+        if recipient != '' and donor_name != '' and amt > 0 and other_id   == '':
+            return [recipient, donor_name, zip_code, year, amt]
+        else:
+            return None
+        
+    except:
+        return None
 
 if __name__ == "__main__":
 
@@ -16,80 +35,82 @@ if __name__ == "__main__":
     f = open(filename)
     data = f.readlines()    
     repeatedDonor = {} # repeat donor
-    resipientRecord = {} # repeated transaction
+    recipientRecord = {} # repeated transaction
     
     f2 = open(sys.argv[2])
     
-    p = int(f2.read())
+    try:
+        p = int(f2.read())
+    except:
+        print "percentile reading error..terminating"
+        sys.exit()
     
     output_file = open(sys.argv[3],"w")
     
     for line in data:
-        record = line.split('|')
-        id = record[0]
-        name = record[7]
-        zip = record[10]
-        date = record[13]
-        amt = record[14]
-        other_id = record[15]
-        
-        if id != '' and name != '' and zip != '' and len(zip) > 4 and date != '' and amt != 0 and other_id   == '':
-            
-            name_and_zip = name+"|"+zip[:5]
-            
-            if name_and_zip in repeatedDonor: #Its a repeated donor
-                                                    
-                # add the current record for the repeated donor
                 
-                resipient_zip_year = id+"|"+zip[:5]+"|"+date[-4:]
-                
-                if resipient_zip_year in resipientRecord:
+        vals = getValues(line)
 
-                    heap, sum_amt, cnt = resipientRecord[resipient_zip_year]
-                    #b.append(int(amt))
-                    heapq.heappush(heap, int(amt))
-                    cnt += 1
-                    sum_amt += int(amt)
-                    resipientRecord[resipient_zip_year] = [heap,sum_amt,cnt ]
-                else:
-                    heap = [int(amt)]
-                    cnt = 1
-                    sum_amt = int(amt)
-                    resipientRecord[resipient_zip_year] = [heap,sum_amt,cnt ]               
-                                                
-                
-                id1 , zip1, date1 , amt1, seen = repeatedDonor[name_and_zip].split("|") # find the first record of this donor
-
-                if seen == "False": # first record has not been seen, add the first record
-                                        
-                     resipient_zip_year1 = id1+"|"+zip1[:5]+"|"+date1[-4:] #make the key for the first record
+        if(vals):
+            recipient, donor_name, zip_code, year, amt = vals
+        else:
+            print "Skipping", line
+            continue
                     
-                     if resipient_zip_year1 in resipientRecord:   # check if this key already exists in the transactions
-                         heap, sum_amt, cnt = resipientRecord[resipient_zip_year1]
-                         #b.append(int(amt1))
-                         heapq.heappush(heap, int(amt1))
-                         cnt += 1
-                         sum_amt += int(amt1)
-                         resipientRecord[resipient_zip_year1] = [heap,sum_amt,cnt] #append the first record value
-                     else:
-                         heap = [int(amt1)]
-                         cnt = 1
-                         sum_amt = int(amt1)
-                         resipientRecord[resipient_zip_year1] = [heap,sum_amt,cnt ] #make the first entry
+        name_and_zip = donor_name+"|"+str(zip_code)
+            
+        if name_and_zip in repeatedDonor: #Its a repeated donor
+                                                    
+             # add the current record for the repeated donor
+                
+            recipient_zip_year = recipient+"|"+ str(zip_code)+"|"+str(year)
+                
+            if recipient_zip_year in recipientRecord:
+
+                heap, sum_amt, cnt = recipientRecord[recipient_zip_year]
+
+                heapq.heappush(heap, amt)
+                cnt += 1
+                sum_amt += amt
+                recipientRecord[recipient_zip_year] = [heap,sum_amt,cnt ]
+            else:
+                heap = [amt]
+                cnt = 1
+                sum_amt = amt
+                recipientRecord[recipient_zip_year] = [heap,sum_amt,cnt ]                                                               
+            
+            recipient1 , zip_code1, year1 , amt1, seen = repeatedDonor[name_and_zip].split("|")
+	    # find the first record of this donor
+
+            if seen == "False": # first record has not been seen, add the first record
+                                    
+                 recipient_zip_year1 = recipient1+"|"+zip_code1+"|"+year1 #make the key for the first record
+                
+                 if recipient_zip_year1 in recipientRecord:   # check if this key already exists in the transactions
+                     heap, sum_amt, cnt = recipientRecord[recipient_zip_year1]
+                     heapq.heappush(heap, int(amt1))
+                     cnt += 1
+                     sum_amt += int(amt1)
+                     recipientRecord[recipient_zip_year1] = [heap,sum_amt,cnt] #append the first record value
+                 else:
+                     heap = [int(amt1)]
+                     cnt = 1
+                     sum_amt = int(amt1)
+                     recipientRecord[recipient_zip_year1] = [heap,sum_amt,cnt ] #make the first entry
                         
-                     repeatedDonor[name_and_zip] = id1 +"|"+zip1+"|"+date1+"|"+amt1+"|"+"True" #change the value to be seen 
+                 repeatedDonor[name_and_zip] = recipient_zip_year1+"|"+amt1+"|"+"True" #change the value to be seen 
                 
-                heap,sum_amt,cnt = resipientRecord[resipient_zip_year]
+            heap,sum_amt,cnt = recipientRecord[recipient_zip_year]
                 
-                percentile_p = str(heapq.heappop(heap))#str(percentile(heap,p))
+            percentile_p = str(percentile(heap,p))
                 
-                output = resipient_zip_year+"|"+percentile_p+"|"+str(sum_amt)+"|"+str(cnt)+"\n"
-                output_file.write(output)
+            output = recipient_zip_year+"|"+percentile_p+"|"+str(sum_amt)+"|"+str(cnt)+"\n"
+            output_file.write(output)
                 #print output
                 
-            else:
+        else:
                 
-                repeatedDonor[name_and_zip] = id +"|"+zip+"|"+date+"|"+amt+"|"+"False" #has not been seen
+            repeatedDonor[name_and_zip] = recipient +"|"+ str(zip_code) +"|"+ str(year) +"|"+ str(amt) +"|"+"False" #has not been seen
                 
 
 output_file.close()                    
